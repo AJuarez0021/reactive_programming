50edfb90d0a24e2c3ac188e3f2ef2e11ff7f6746
A. Juarez <ajuarez0014@gmail.com>
2022-04-19 19:28:39 -0500

    Se quitan archivos innecesarios

--- spring-reactor-demo/docker/deploy.sh
+++ spring-reactor-demo/docker/deploy.sh
@@ -7,7 +7,7 @@ echoYellow() { echo $'\e[0;33m'"$1"$'\e[0m'; }
 
 build(){
   mvn clean package
-  docker build -f Dockerfile -t academy:latest .
+  docker build -f Dockerfile -t reactor:latest .
   docker-compose up
 }
 
